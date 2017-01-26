2ca6eb793ba16b9e997e31f7522ba869cab04961
Jeffrey Gonzalez <jgonzalez1@ucvts.org>
2017-01-26 02:54:48 +0000

    Final Project

---
+++ scatterplot2.py
@@ -0,0 +1,48 @@
+#import stuff for the weather api and for charting 
+import requests
+import pandas as pd
+import csv
+
+#create the url
+endpoint = 'https://api.darksky.net/forecast/'
+key = 'c4be34991425bf8b6c828a21288faefc'
+lat = '40.6640'
+lon = '74.2107'
+year = int(input("pick a year "))
+time = str(year) + '-11-23T15:00:00'
+
+
+#assemble full url
+url = endpoint + key + '/' + lat + ',' + lon + ',' + time
+
+#call the url
+r = requests.get(url)
+
+weather = r.json()
+
+#create the csv
+csvfile = open('scatterplot2.csv', 'w')
+csvwriter = csv.writer(csvfile, delimiter = ',')
+csvwriter.writerow(['Date', 'Temperature'])
+
+#make the data
+for x in range(year,2016):
+    #assemble full url
+    time = str(x) + '-06-19T15:00:00'
+    url = endpoint + key + '/' + lat + ',' + lon + ',' + time
+    r = requests.get(url)
+    weather = r.json()
+    Temperature = weather['currently']['temperature']
+    csvwriter.writerow([x, Temperature])
+#finish the csvfile
+csvfile.close()    
+
+#set up bokeh for the graph
+import bokeh 
+from bokeh.charts import Scatter, output_file, save
+df = pd.read_csv('scatterplot2.csv')
+p = Scatter(df, x = 'Date', y = 'Temperature', color = 'blue', title = "Friend's Birthday Temperature", legend = 'top_right', xlabel = "Year", ylabel = "Temperature")
+
+#save the file
+output_file('scatterplot2.html')
+save(p)
---
+++ vis-ideas.md
@@ -0,0 +1,5 @@
+So I'm planning on using the darksky api to be able to find the temperatures
+that it's been on my birthday for the past 16 years at 3:00 pm in Elizabeth.
+For my visual representations, I believe that I could make a scatterplot for the temperatures over the years,
+but also a scatterplot of someone else's birthday to measure and compare the range 
+of different temperatures over the past 16 years.
